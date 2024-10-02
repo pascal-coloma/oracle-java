acaf9dae0e68d7bf4baa9d0f6b72b1c69ac63021
Sade995 <pas.coloma@duocuc.cl>
2024-10-02 08:27:01 -0300

    Update on main and Clothing classes

---
+++ ShopApp/src/duke/choice/Clothing.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duke.choice;
+
+/**
+ *
+ * @author colom
+ */
+public class Clothing {
+    
+    private String description, size;
+    private double price;
+
+    public Clothing() {
+    }
+
+    public Clothing(String description, double price, String size) {
+        this.description = description;
+        this.price = price;
+        this.size = size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Description: " + description + "\nSize: " + size + "\nPrice: " + price;
+    }
+    
+    
+
+   
+    
+}
--- ShopApp/src/duke/choice/ShopApp.java
+++ ShopApp/src/duke/choice/ShopApp.java
@@ -14,10 +14,21 @@ public class ShopApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        double tax = 0.2, total;
         System.out.println("Welcome to Duke Choice Shop!");
-        
         Customer c1 = new Customer("Pinky");
+        Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
+        Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
+        
         System.out.println("Hi! " + c1.getName());
+        System.out.println("Purchase details");
+        System.out.println(item1.toString());
+        System.out.println(item2.toString());
+        System.out.println("------------------------------------");
+        System.out.println("Pre tax: " + (item1.getPrice()  + item2.getPrice()* 2) );
+        total = (item1.getPrice() + item2.getPrice() * 2) * (1 + tax);
+        System.out.println("Total: " + total);
+        
     }
     
 }
