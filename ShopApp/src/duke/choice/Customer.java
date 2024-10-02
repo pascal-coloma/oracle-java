a4ab6d1eada0b6d520be79795c50987b2ce66d6e
pascal-coloma <pas.coloma@duocuc.cl>
2024-10-02 07:54:32 -0300

    First build of classes and atributes
    Assignments required by the Java Fundametals Oracle Learn course.

---
+++ ShopApp/src/duke/choice/Customer.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package duke.choice;
+
+/**
+ *
+ * @author colom
+ */
+public class Customer {
+    
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + "name=" + name + '}';
+    }
+    
+    
+}
---
+++ ShopApp/src/duke/choice/ShopApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package duke.choice;
+
+/**
+ *
+ * @author colom
+ */
+public class ShopApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Duke Choice Shop!");
+        
+        Customer c1 = new Customer("Pinky");
+        System.out.println("Hi! " + c1.getName());
+    }
+    
+}
